ba47d0d62c2d340f03327a4c9fea936807d7a979
Burak Kara <burakkara35@gmail.com>
2021-03-16 21:14:38 +0300

    Refactoring on Character Controller

---
+++ PlatformRunner_Demo/Assets/Scripts/Character/Character.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Handler))]
+public class Character : MonoBehaviour
+{
+    private State _currentState;
+    private State _newState;
+    private Handler _handler;
+
+    [HideInInspector] public Rigidbody rigidbody;
+    [HideInInspector] private Animator animator;
+    [HideInInspector] public GameManager game;
+
+    public float runSpeed;
+    public float moveSideSpeed;
+
+    public enum CharacterMovingSideState { None, Left, Right }
+    [HideInInspector] public CharacterMovingSideState characterMovingSideState;
+
+    private enum Event { Enter, Update }
+    private Event _event;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        game = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+
+        _handler = GetComponent<Handler>();
+    }
+
+    private void Start()
+    {
+        InitializeState();
+
+        Invoke("TempStartGame", 3f);
+    }
+
+    void TempStartGame()
+    {
+        game.gameState = GameManager.GameState.On;
+    }
+
+    private void Update()
+    {
+        StateMachine();
+        ControlIfStateChanged();
+
+        if (_event == Event.Enter)
+        {
+            _currentState.Enter();
+            _event = Event.Update;
+        }
+        else if (_event == Event.Update)
+            _currentState.LogicUpdate();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_event == Event.Update)
+            _currentState.PhysicsUpdate();
+    }
+
+    private void StateMachine()
+    {
+        if (game.gameState == GameManager.GameState.Off)
+        {
+            _newState = new Idle(rigidbody, animator);
+        }
+        else
+        {
+            _handler.Handle();
+
+            if (characterMovingSideState == CharacterMovingSideState.Left)
+                _newState = new MoveLeft(rigidbody, moveSideSpeed, runSpeed);
+            else if (characterMovingSideState == CharacterMovingSideState.Right)
+                _newState = new MoveRight(rigidbody, moveSideSpeed, runSpeed);
+            else
+                _newState = new Run(rigidbody, animator, runSpeed);
+        }
+    }
+
+    private void ControlIfStateChanged()
+    {
+        if (_currentState.ToString() != _newState.ToString())
+        {
+            _currentState = _newState;
+            _event = Event.Enter;
+        }
+    }
+
+    public void InitializeState()
+    {
+        _currentState = new Idle(rigidbody, animator);
+        _event = Event.Enter;
+    }
+}
---
+++ PlatformRunner_Demo/Assets/Scripts/Character/InputHandler.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Character))]
+public class InputHandler : MonoBehaviour, Handler
+{
+
+    #region Input Variables
+    private bool _swipe = false;
+    private bool _swipeFinished = true;
+    private float _swipeFirstPosition;
+    private float _differenceBetweenSwipePositions;
+    private float _swipingInSeconds = 0.1f;
+    private IEnumerator _swipeCoroutine;
+    #endregion
+
+    Character character;
+
+    private void Awake()
+    {
+        character = GetComponent<Character>();
+    }
+
+    public void Handle()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _swipe = true;
+            _swipeFirstPosition = Input.mousePosition.x;
+            _swipeCoroutine = Swiping();
+            StartCoroutine(_swipeCoroutine);
+        }
+        if (Input.GetMouseButton(0) && _swipe)
+        {
+            _swipeFinished = false;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            _swipe = false;
+            _swipeFinished = true;
+            character.characterMovingSideState = Character.CharacterMovingSideState.None;
+            StopCoroutine(_swipeCoroutine);
+        }
+    }
+
+    private IEnumerator Swiping()
+    {
+        yield return new WaitForSeconds(_swipingInSeconds);
+        if (_swipe)
+        {
+            _differenceBetweenSwipePositions = Input.mousePosition.x - _swipeFirstPosition;
+
+            if (_differenceBetweenSwipePositions < 0)
+                character.characterMovingSideState = Character.CharacterMovingSideState.Left;
+            else if (_differenceBetweenSwipePositions > 0)
+                character.characterMovingSideState = Character.CharacterMovingSideState.Right;
+
+            if (!_swipeFinished)
+            {
+                _swipeCoroutine = Swiping();
+                StartCoroutine(_swipeCoroutine);
+            }
+        }
+    }
+}
