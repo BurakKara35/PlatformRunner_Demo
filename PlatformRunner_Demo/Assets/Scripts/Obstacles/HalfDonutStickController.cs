6fb8312c8ce92889f5cbf6d07eae06231620448d
Burak Kara <burakkara35@gmail.com>
2021-03-18 13:10:39 +0300

    Half Donut obstacle correction

--- PlatformRunner_Demo/Assets/Scripts/Obstacles/HalfDonutStickController.cs
+++ PlatformRunner_Demo/Assets/Scripts/Obstacles/HalfDonutStickController.cs
@@ -1,25 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class HalfDonutStickController : HorizontalMovingObstaclesBase
 {
+    private enum FacingSide { Left, Right}
+    private FacingSide _facingSide;
+
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
         _boundryInX = 0.145f;
 
+        CheckObstacleFacingSide();
         ChooseDirectionFirst();
     }
 
     private void Update()
     {
-        CheckPosition(transform.localPosition.x);
+        if (_facingSide == FacingSide.Left)
+            CheckPosition(transform.localPosition.x);
+        else
+            CheckPosition(-transform.localPosition.x);
     }
 
     private void FixedUpdate()
     {
         Movement();
+    }
+
+    private void CheckObstacleFacingSide()
+    {
+        if (transform.parent.transform.eulerAngles.y == 180)
+            _facingSide = FacingSide.Right;
+        else
+            _facingSide = FacingSide.Left;
     }
 }
